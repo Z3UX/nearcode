9d7e41a963888da232a639b57ef79872e49badbb
Josué Almeida <josue.almeida@gmail.com>
2020-04-27 11:09:08 +0100

    Started Spring Boot project

---
+++ src/test/java/com/nearcodeconsulting/exercise/ExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nearcodeconsulting.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
